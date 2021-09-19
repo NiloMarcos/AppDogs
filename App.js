acc4c0240c37c2b8684dbdd848e3177b4e0d022f
Nilo Marcos <nilo.n2m15@gmail.com>
2021-09-18 22:09:05 -0300

    implement of fonts

--- App.js
+++ App.js
@@ -2,10 +2,30 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes';
 
+import AppLoading from 'expo-app-loading';
+import {useFonts} from 'expo-font';
+import {
+  Poppins_400Regular, 
+  Poppins_400Regular_Italic, 
+  Poppins_700Bold, 
+  Poppins_700Bold_Italic } from '@expo-google-fonts/poppins';
+
 export default function App() {
-  return(
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
-  );
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular, 
+    Poppins_400Regular_Italic, 
+    Poppins_700Bold, 
+    Poppins_700Bold_Italic
+  });
+  
+  if(!fontsLoaded){
+    return <AppLoading />;
+  } else {
+    return(
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    );
+  }
+
 }
